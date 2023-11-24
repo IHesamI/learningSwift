daba8beb1c9de52dc7862d4b41ba1029c56ee51f
hesam <bhesam2000@yahoo.com>
2023-11-24 13:52:03 +0330

    use icon and text

--- weatherApp/weatherApp.swift
+++ weatherApp/weatherApp.swift
@@ -11,6 +11,20 @@ struct ContentView: View {
                 Text("Hesam")
                     .font(.system(size:32 , weight:.medium , design: .default))
                     .forgegroundColor(.white)
+                    .padding(.bottom,200)
+
+                VStack {
+                    Image(systemName:"cloud.sun.fill")
+                        .renderingMode(.original)
+                        .resizeable()
+                        .aspectRatio(contentMode:.fit)
+                        .frame(width:180,height:180)
+
+                    Text("76*")
+                        .font(.system(size:70,weight:.medium))
+                        .forgegroundColor(.white)
+                }
+                Spacer()
             }
 
         }
